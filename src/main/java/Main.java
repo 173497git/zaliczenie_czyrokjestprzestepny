af074edb6fd3b239bdefc91d562fa5b86a9aaac3
173497git <161622376+173497git@users.noreply.github.com>
2024-06-19 14:09:59 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Podaj rok:  ");
+    int rok = scanner.nextInt();
+    if (rok % 4 == 0 && rok % 100 != 0 || rok % 400 == 0  ) {
+      System.out.println("Rok " + rok + " jest przestępny");
+    }
+    else {
+      System.out.println("Rok " + rok + " nie jest przestępny");
+    }
+  
+   
+  }
+
+
+}
